a1368b95ef531d5465aa8dc7dc638132c434c5f2
cyberguli <cyberguli@cli-10-110-34-118.WIFIeduroamSTUD.units.it>
2024-02-22 18:08:09 -0500

    added tree

---
+++ README.md
@@ -0,0 +1,11 @@
+# Darcy Flow Non intrusive ROM Analysis
+In this repo, some common used models are compared on a [Navier](https://github.com/guglielmopadula/PeriodicNavierStokes) [Stokes](https://github.com/guglielmopadula/PeriodicNavierStokes) [Dataset](https://github.com/guglielmopadula/PeriodicNavierStokes).
+
+
+The models, with their main characteristics and 
+performances, are summed up here.
+
+
+|Model              |rel u train error|rel u test error super| 
+|-------------------|-----------------|----------------------|
+|POD+Tree           |1.8e-15          |0.08                | 
---
+++ tree_pod.py
@@ -0,0 +1,175 @@
+import numpy as np
+from ezyrb import RBF, GPR, POD
+from sklearn.tree import DecisionTreeRegressor
+import scipy.linalg
+from periodicnavierstokes import PeriodicNavierStokes
+from tqdm import trange
+import torch.nn as nn
+import torch
+import numpy as np
+from tqdm import trange
+np.random.seed(0)
+batch_size=10
+data=PeriodicNavierStokes(batch_size)
+points=data.points_red[:,:-1]
+time=data.time_red
+points_super=data.points.reshape(128*128,3)[:,:-1]
+time_super=data.time
+all_points=torch.concatenate((time.unsqueeze(0).unsqueeze(-1).repeat(4096,1,1),points.unsqueeze(-2).repeat(1,50,1)),dim=2)
+all_points=all_points.reshape(-1,3)
+all_points_super=torch.concatenate((time_super.unsqueeze(0).unsqueeze(-1).repeat(128*128,1,1),points_super.unsqueeze(-2).repeat(1,100,1)),dim=2)
+all_points_super=all_points_super.reshape(-1,3)
+u_super=data.U_super
+a_super=data.A_super
+u_=data.U_test
+a_test=data.A_test
+a_train,u_train=data.train_dataset.tensors
+a_test,u_test=data.test_dataset.tensors
+
+
+a_test=a_test.unsqueeze(0)
+u_test=u_test.unsqueeze(0)
+a_super=a_super.unsqueeze(0)
+u_super=u_super.unsqueeze(0)
+u_train=u_train.reshape(u_train.shape[0],-1)
+a_train=a_train.reshape(u_train.shape[0],-1)
+u_test=u_test.reshape(u_test.shape[0],-1)
+a_test=a_test.reshape(u_test.shape[0],-1)
+u_super=u_super.reshape(u_super.shape[0],-1)
+a_super=a_super.reshape(u_super.shape[0],-1)
+a_train=a_train.unsqueeze(-1).repeat(1,1,50).reshape(a_train.shape[0],-1)
+a_test=a_test.unsqueeze(-1).repeat(1,1,50).reshape(a_test.shape[0],-1)
+
+points=all_points
+points_super=all_points_super
+
+class Operator():
+    def __init__(self,m,p): #m is the number of sensors
+        self.model_trunk=DecisionTreeRegressor()
+        self.model_branch=DecisionTreeRegressor()
+        self.pod=POD(method='randomized_svd',rank=p)
+        self.m=m
+        self.p=p
+    
+    def fit(self,x,y,points):
+        self.pod.fit(y)
+        self.modes=self.pod.modes
+        self.basis=self.pod.transform(y)
+        self.indices=np.random.permutation(self.basis.shape[1])[:self.m]
+        self.model_branch.fit(x[:,self.indices],self.modes)
+        self.model_trunk.fit(points,self.basis.T)
+
+    def predict(self,x,points):
+        x_red=x[:,self.indices]
+        return self.model_branch.predict(x_red)@(self.model_trunk.predict(points).T)
+    
+
+model=Operator(p=2000,m=1000)
+
+a_train=a_train.numpy()
+points=points.numpy()
+u_train=u_train.numpy()
+a_test=a_test.numpy()
+u_test=u_test.numpy()
+a_super=a_super.numpy()
+u_super=u_super.numpy()
+
+points=points
+model.fit(a_train,u_train,points)
+print(a_test.shape)
+print(a_train.shape)
+
+u_pred_test=model.predict(a_test,points)
+u_pred_super=model.predict(a_test,points_super)
+u_pred_train=model.predict(a_train,points)
+
+
+
+loss_train=np.mean(np.linalg.norm(u_train-u_pred_train,axis=1)/np.linalg.norm(u_train,axis=1))
+loss_test=np.mean(np.linalg.norm(u_test-u_pred_test,axis=1)/np.linalg.norm(u_test,axis=1))
+loss_super=np.mean(np.linalg.norm(u_super-u_pred_super,axis=1)/np.linalg.norm(u_super,axis=1))
+
+print(loss_train)
+print(loss_test)
+print(loss_super)
+
+u=u_super[0].reshape(128,128,100)
+pred=u_pred_super[0].reshape(128,128,100)
+
+
+import matplotlib.pyplot as plt
+plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
+import matplotlib.animation as animation
+import os
+import sys
+
+print(a_super.shape)
+fig, axarr = plt.subplots(1,2,figsize=(16,8))
+
+
+
+
+nSeconds = 2
+fps=50
+times=time_super.cpu().numpy()
+name=os.path.splitext(os.path.basename(sys.argv[0]))[0]
+u_all=u
+u_rec_all=pred
+
+err=u_all-u_rec_all
+print(np.min(u))
+print(np.max(u))
+print(np.min(u_rec_all))
+print(np.max(u_rec_all))
+
+print(np.min(err))
+print(np.max(err))
+
+fig, axarr = plt.subplots(1,3,figsize=(24,8))
+a=axarr[0].imshow(u_all[:,:,0],interpolation='none', aspect='auto', vmin=-0.7, vmax=3.3, cmap="inferno")
+b=axarr[1].imshow(u_rec_all[:,:,0],interpolation='none', aspect='auto', vmin=-0.7, vmax=3.3,cmap="inferno")
+c=axarr[2].imshow(err[:,:,0],interpolation='none', aspect='auto', vmin=-0.7, vmax=3.3, cmap="inferno")
+axarr[0].set(xlabel='orig')
+axarr[1].set(xlabel='rec')
+axarr[2].set(xlabel='err')
+
+def animate_func(i):
+    a.set_array(u_all[:,:,i])
+    b.set_array(u_rec_all[:,:,i])
+    fig.suptitle('t='+str(times[i]))
+    c.set_array(err[:,:,i])
+    return [a,b,c]
+
+anim = animation.FuncAnimation(
+                               fig, 
+                               animate_func, 
+                               frames = nSeconds * fps,
+                               interval = 1000 / fps, # in ms
+                               )
+
+anim.save('videos/u_'+name+'.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
+
+
+fig, axarr = plt.subplots(1,2,figsize=(16,8))
+a=axarr[0].imshow(u_all[:,:,0],interpolation='none', aspect='auto', vmin=-0.7, vmax=3.3, cmap="inferno")
+b=axarr[1].imshow(u_rec_all[:,:,0],interpolation='none', aspect='auto', vmin=-0.7, vmax=3.3,cmap="inferno")
+axarr[0].set(xlabel='orig')
+axarr[1].set(xlabel='rec')
+
+
+def animate_func(i):
+    a.set_array(u_all[:,:,i])
+    b.set_array(u_rec_all[:,:,i])
+    fig.suptitle('t='+str(times[i]))
+    c.set_array(err[:,:,i])
+    return [a,b,c]
+
+anim = animation.FuncAnimation(
+                               fig, 
+                               animate_func, 
+                               frames = nSeconds * fps,
+                               interval = 1000 / fps, # in ms
+                               )
+
+anim.save('videos/u_no_err'+name+'.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
+
